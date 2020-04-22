ad2e56a03f11763eb1476362dfa7517f9f09d918
Anderson Colossi <cocalense@gmail.com>
2020-04-21 23:16:21 -0300

    Condição Simples

--- aula 11/ex008.js
+++ aula 11/ex008.js
@@ -2,9 +2,11 @@
 
 var vel = 50
 console.log(`A velocidade do carro é de ${vel} km/h.`)
+
 if(vel > 60){
     console.log('Voce ultrapassou o limite de velocidade, MULTA !')
 }
 else{
     console.log('Boa viagem !')
+
 }
