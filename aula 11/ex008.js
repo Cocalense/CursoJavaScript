01e745071fe7fec5629d01d7fde75a9853022ebd
Anderson Colossi <cocalense@gmail.com>
2020-04-21 23:08:00 -0300

    Create ex008.js

---
+++ aula 11/ex008.js
@@ -0,0 +1,10 @@
+//console.log('O console funcionou corretamente !')
+
+var vel = 50
+console.log(`A velocidade do carro é de ${vel} km/h.`)
+if(vel > 60){
+    console.log('Voce ultrapassou o limite de velocidade, MULTA !')
+}
+else{
+    console.log('Boa viagem !')
+}
