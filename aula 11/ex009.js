82025c3cc5376341fe84e06ebc7ca0da7750f293
Anderson Colossi <cocalense@gmail.com>
2020-04-21 23:15:41 -0300

    Condição Composta

---
+++ aula 11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Portugal'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil'){
+    console.log('Você é cidadão Brasileiro')
+}else{
+    console.log('Você não é cidadão Brasileiro, logo é Estrangeiro !')
+}
