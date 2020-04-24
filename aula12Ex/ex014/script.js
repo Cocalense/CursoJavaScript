090a32a72728e08ee9e04e52c634fa6080a783e5
Anderson Colossi <cocalense@gmail.com>
2020-04-24 00:46:36 -0300

    aula12Ex\config. JS

---
+++ aula12Ex/ex014/script.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && < 12){
+        //Bom dia
+        img.src = 'fotomanha.png'
+    }else if(hora >= 12 && hora < 18){
+        //Boa tarde
+        img.src = 'fototarde.png'
+    }else{
+        //Boa noite
+        img.src = 'fotonoite.png'
+    }
+}
+
